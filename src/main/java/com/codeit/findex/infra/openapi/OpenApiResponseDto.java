9493336ae54661832228e95f5d2919be685ac401
Jerong81 <jhdb96@gmail.com>
2026-05-18 12:27:06 +0900

    feat: [KAN-24] Open API 응답 DTO 필드 추가

--- src/main/java/com/codeit/findex/infra/openapi/OpenApiResponseDto.java
+++ src/main/java/com/codeit/findex/infra/openapi/OpenApiResponseDto.java
@@ -56,9 +56,14 @@ public class OpenApiResponseDto {
         private String trqu; // 거래량
         private String trPrc; // 거래대금
         private String lstgMrktTotAmt; // 상장시가총액
+        private String lsYrEdVsFltRg; // 전년말대비등락폭
+        private String lsYrEdVsFltRt; // 전년말대비등락률
+        private String yrWRcrdHgst; // 연중최고
+        private String yrWRcrdHgstDt; // 연중최고일자
+        private String yrWRcrdLwst; // 연중최저
+        private String yrWRcrdLwstDt; // 연중최저일자
         private String basPntm; // 기준시점
         private String basIdx; // 기준지수
-
 
     }
 }
