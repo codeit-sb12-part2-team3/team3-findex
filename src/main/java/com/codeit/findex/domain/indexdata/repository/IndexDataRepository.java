a23dd30604abbe1b7b49c67ab5cbf7fb9b7ba168
Woojin-Jeong <dnwls3958@naver.com>
2026-05-19 13:24:54 +0900

    fix: IndexDataRepository 타입 및 메소드 수정

--- src/main/java/com/codeit/findex/domain/indexdata/repository/IndexDataRepository.java
+++ src/main/java/com/codeit/findex/domain/indexdata/repository/IndexDataRepository.java
@@ -4,12 +4,7 @@ import com.codeit.findex.domain.indexdata.entity.IndexData;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 import java.util.UUID;
 
-
-public interface IndexDataRepository extends JpaRepository<IndexData, Long> {
-
-    Optional<IndexData> findById(UUID id);
-
+public interface IndexDataRepository extends JpaRepository<IndexData, UUID> {
 }
