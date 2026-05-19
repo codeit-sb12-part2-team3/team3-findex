d849fce8ca94afd44357f2304bfca6dfafab4302
hyeon2628 <rygus0128@gmail.com>
2026-05-19 15:44:58 +0900

    chore: 중복 UUID import 제거

--- src/main/java/com/codeit/findex/domain/indexinfo/controller/IndexInfoController.java
+++ src/main/java/com/codeit/findex/domain/indexinfo/controller/IndexInfoController.java
@@ -11,8 +11,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
-import java.util.UUID;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/index-infos")
