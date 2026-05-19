fafd6fb633bed7f44cfe4334519a89c7fae77daa
hyeon1226 <rygus0128@gmail.com>
2026-05-19 11:51:38 +0900

    feat: 지수 정보 목록/단건 조회 기능 구현

--- src/main/java/com/codeit/findex/domain/indexinfo/service/IndexInfoService.java
+++ src/main/java/com/codeit/findex/domain/indexinfo/service/IndexInfoService.java
@@ -4,9 +4,14 @@ import com.codeit.findex.domain.indexinfo.dto.IndexInfoCreateRequest;
 import com.codeit.findex.domain.indexinfo.dto.IndexInfoResponse;
 import com.codeit.findex.domain.indexinfo.entity.IndexInfo;
 import com.codeit.findex.domain.indexinfo.repository.IndexInfoRepository;
+import com.codeit.findex.global.exception.BusinessException;
+import com.codeit.findex.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -34,17 +39,40 @@ public class IndexInfoService {
         IndexInfo savedIndexInfo = indexInfoRepository.saveAndFlush(indexInfo);
 
         // Entity -> Response DTO 변환
+        return toResponse(savedIndexInfo);
+    }
+    // 지수 정보 목록 조회
+    @Transactional(readOnly = true)
+    public List<IndexInfoResponse> findAll() {
+        return indexInfoRepository.findAll()
+                .stream()
+                .map(this::toResponse)
+                .toList();
+    }
+
+    // 지수 정보 단건 조회
+    @Transactional(readOnly = true)
+    public IndexInfoResponse findById(UUID id) {
+        IndexInfo indexInfo = indexInfoRepository.findById(id)
+                // 데이터가 없을 경우 공통 예외 처리
+                .orElseThrow(() -> new BusinessException(ErrorCode.NOT_FOUND));
+        return toResponse(indexInfo);
+    }
+
+
+    // Entity -> Response DTO 변환
+    private IndexInfoResponse toResponse(IndexInfo indexInfo) {
         return new IndexInfoResponse(
-                savedIndexInfo.getId(),
-                savedIndexInfo.getIndexName(),
-                savedIndexInfo.getIndexClassification(),
-                savedIndexInfo.getEmployedItemsCount(),
-                savedIndexInfo.getBasePointInTime(),
-                savedIndexInfo.getBaseIndex(),
-                savedIndexInfo.getSourceType(),
-                savedIndexInfo.getFavorite(),
-                savedIndexInfo.getCreatedAt(),
-                savedIndexInfo.getUpdatedAt()
+                indexInfo.getId(),
+                indexInfo.getIndexName(),
+                indexInfo.getIndexClassification(),
+                indexInfo.getEmployedItemsCount(),
+                indexInfo.getBasePointInTime(),
+                indexInfo.getBaseIndex(),
+                indexInfo.getSourceType(),
+                indexInfo.getFavorite(),
+                indexInfo.getCreatedAt(),
+                indexInfo.getUpdatedAt()
         );
     }
 }
