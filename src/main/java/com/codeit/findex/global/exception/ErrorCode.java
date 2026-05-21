2761642ea5f4b3d9d1f55fd47986b3533dbf77a3
Woojin-Jeong <dnwls3958@naver.com>
2026-05-21 10:08:46 +0900

    feat: 공통 예외처리 및 API 응답 구조 통일 구현

--- src/main/java/com/codeit/findex/global/exception/ErrorCode.java
+++ src/main/java/com/codeit/findex/global/exception/ErrorCode.java
@@ -19,9 +19,24 @@ public enum ErrorCode {
             "요청한 데이터를 찾을 수 없습니다."
     ),
 
+    UNSUPPORTED_MEDIA_TYPE(
+            HttpStatus.UNSUPPORTED_MEDIA_TYPE,
+            "지원하지 않는 Content-Type입니다."
+    ),
+
     INTERNAL_SERVER_ERROR(
             HttpStatus.INTERNAL_SERVER_ERROR,
             "서버 내부 오류가 발생했습니다."
+    ),
+
+    GATEWAY_TIMEOUT(
+            HttpStatus.GATEWAY_TIMEOUT,
+            "외부 API 서버 응답 시간이 초과되었습니다."
+    ),
+
+    METHOD_NOT_ALLOWED(
+            HttpStatus.METHOD_NOT_ALLOWED,
+            "지원하지 않는 HTTP 메서드 입니다."
     ),
 
     // Open API 관련 에러
--- src/main/java/com/codeit/findex/global/exception/ErrorResponse.java
+++ src/main/java/com/codeit/findex/global/exception/ErrorResponse.java
@@ -14,4 +14,7 @@ public class ErrorResponse {
 
     private String message;
 
+    public static ErrorResponse of(ErrorCode errorCode) {
+        return new ErrorResponse(errorCode.name(), errorCode.getMessage());
+    }
 }
